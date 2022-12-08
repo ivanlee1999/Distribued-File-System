06b713841c7e5747dfd8d310373e69747c02e2e5
ivanlee1999 <liyifan3829@outlook.com>
2022-12-08 17:30:45 -0600

    modified server

--- server.c
+++ server.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include "udp.h"
 #include "signal.h"
+#include "structure.h"
 
 #define BUFFER_SIZE (1000)
 
 int sd;
+int fd;
 
 void intHandler(int dummy) {
     UDP_Close(sd);
@@ -12,25 +14,76 @@ void intHandler(int dummy) {
 }
  
 // server code
-int main(int argc, char *argv[]) {
-    signal(SIGINT, intHandler);
-    sd = UDP_Open(10000);
+// int main(int argc, char *argv[]) {
+//     signal(SIGINT, intHandler);
+//     sd = UDP_Open(10000);
+//     assert(sd > -1);
+//     while (1) {
+// 	struct sockaddr_in addr;
+// 	Msg* request = malloc(sizeof(Msg));
+// 	printf("server:: waiting...\n");
+// 	int rc = UDP_Read(sd, &addr, request, BUFFER_SIZE);
+// 	printf("server:: read message [size:%d contents:(%s)]\n", rc, request);
+
+
+
+// 	if (rc > 0) {
+//             char reply[BUFFER_SIZE];
+//             sprintf(reply, "goodbye CS537");
+//             rc = UDP_Write(sd, &addr, reply, BUFFER_SIZE);
+// 	    printf("server:: reply\n");
+// 	} 
+//     }
+//     return 0; 
+// }
+    
+
+int shutdown(){
+    fsync(fd);
+    exit(0);
+}
+
+
+int startServer(int port, char* img){
+    sd = UDP_Open(port);
     assert(sd > -1);
     while (1) {
-	struct sockaddr_in addr;
-	char message[BUFFER_SIZE];
-	printf("server:: waiting...\n");
-	int rc = UDP_Read(sd, &addr, message, BUFFER_SIZE);
-	printf("server:: read message [size:%d contents:(%s)]\n", rc, message);
-	if (rc > 0) {
-            char reply[BUFFER_SIZE];
-            sprintf(reply, "goodbye CS537");
-            rc = UDP_Write(sd, &addr, reply, BUFFER_SIZE);
-	    printf("server:: reply\n");
-	} 
+        struct sockaddr_in addr;
+        Msg* request = malloc(sizeof(Msg));
+        printf("server:: waiting...\n");
+        int rc = UDP_Read(sd, &addr, request, BUFFER_SIZE);
+        printf("server:: read message [size:%d contents:(%s)]\n", rc, request);
+        if (rc > 0) {
+            switch (request->requestType)
+            {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            case 8:
+                shutdown();
+                break;
+            default:
+                break;
+            }
+        } 
     }
     return 0; 
 }
-    
 
+int main(int argc, char *argv[]) {
+    if (argc != 3)
+		perror("Error : Incorrect arguments\n");
 
+	startServer(atoi(argv[1]), argv[2]);
+}
